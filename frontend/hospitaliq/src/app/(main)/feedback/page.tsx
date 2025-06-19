d88cfb5c8200d07c2fcf1d1fc07f1ad6a2ffc637
auxtscin <cindyaustin011206@gmail.com>
2025-06-20 01:54:07 +0800

    Update page.tsx
    Update Feedback Page

--- frontend/hospitaliq/src/app/(main)/feedback/page.tsx
+++ frontend/hospitaliq/src/app/(main)/feedback/page.tsx
@@ -1,7 +1,50 @@
+import { Box, TextField, Button } from "@mui/material"
+
+interface CardProps extends React.PropsWithChildren<{}> {
+  className?: string;
+}
+
+const Card: React.FC<CardProps> = ({ children, className }) => (
+  <Box
+    className={className}
+    sx={{
+      backgroundColor: 'white',
+      borderRadius: '10px',
+      padding: '16px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+      height: '100%',
+      width: '100%',
+    }}
+  >
+    {children}
+  </Box>
+)
+
 export default function FeedbackPage() {
     return (
-        <div>
-            <h1>Feedback</h1>
-        </div>
+        <Box className="bg-primary rounded flex items-center justify-center h-[530px] w-[530px] p-9 ">
+            <Box className="flex flex-col items-start justify-start w-full h-full">
+                    <h4 className="font-bold text-3xl text-white mb-4">Feedback</h4>
+                    <Card className="h-80">
+                        <Box
+      component="form"
+      sx={{ '& .MuiTextField-root': { width: '400px' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="standard-multiline-static"
+          label="Add feedback!"
+          multiline
+          rows={11}
+          variant="standard"
+        />
+      </div>
+    </Box>
+                    </Card>
+                    <Button className="top-10 bg-background rounded-full font-bold text-xs p-3 w-42 ml-38" variant="contained">Submit</Button>
+                </Box>
+        </Box>
     )
 }
